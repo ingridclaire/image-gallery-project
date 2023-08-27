ad3265617082fe3a87c3410cca1687248d0c81e9
PortfolioJoshua <joshuaeirm@gmail.com>
2023-08-26 20:11:13 -0400

    Thanks, tutor.

--- src/routes/category/category-component.jsx
+++ src/routes/category/category-component.jsx
@@ -1,26 +1,70 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories-context';
+import { useContext, useState, Fragment } from 'react';
 import { CategoryContainer } from './category-styles';
-import PhotoImages1 from '../../components/photoimages/photoimages-component'
-import Panel from '../../components/panel/panel-component'
+import { useParams } from 'react-router-dom';
 
-const Category = ({ }) => {
+export const createProductsWithOptions = (arrayOfProducts) => {
+  if (arrayOfProducts) {
+    if (arrayOfProducts.length > 0) {
+      return arrayOfProducts.map(prod => {
+        return {
+          ...prod,
+          options: prod.options || [
+            {option: `option1`, checked: false},
+            {option: `option2`, checked: false},
+            {option: `option3`, checked: false},
+            {option: `option4`, checked: false}
+          ]
+        }
+      })
+    } else {
+      return arrayOfProducts
+    }
+  } else {
+    return arrayOfProducts;
+  }
+}
+
+export default function Category() {
   let { category } = useParams();
-  // console.log("ca: ", { category });
   const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
-  // console.log("category1 :", categoriesMap);
+  const storedProducts = JSON.parse(localStorage.getItem(`products`));
+  const defaultProducts = createProductsWithOptions(categoriesMap["jackets"]);
+  const [products, setProducts] = useState(storedProducts || defaultProducts);
+  console.log(categoriesMap["jackets"])
 
-  
+  function handleCheck(option, product) {
 
-  // console.log("catego: ", categoriesMap);
-  console.log({ products });
+    let productsFromStorage = [];
+    let storedProducts = localStorage.getItem(`products`);
+    if (storedProducts) productsFromStorage = JSON.parse(storedProducts);
 
-  // useEffect(() => {
-  //   console.log(`initialProducts`, categoriesMap[category]);
-  //   // setProducts(products)
-  // }, [])
+    const setCheckedOptionForProducts = (arrayOfProducts) => {
+      if (arrayOfProducts.length > 0) {
+        return arrayOfProducts.map(prod => {
+          if (prod.id === product.id) {
+            return {
+              ...prod,
+              options: prod.options.map(opt => {
+                if (opt.option === option.option) {
+                  opt.checked = !opt.checked;
+                  return opt;
+                } else {
+                  return opt;
+                }
+              })
+            }
+          } else {
+            return prod;
+          }
+        });
+      }
+    }
+
+    let updatedProducts = productsFromStorage.length > 0 ? setCheckedOptionForProducts(productsFromStorage) : setCheckedOptionForProducts(defaultProducts);
+    localStorage.setItem(`products`, JSON.stringify(updatedProducts));
+    setProducts(updatedProducts);
+  }
 
   return (
     <div>
@@ -31,8 +75,21 @@ const Category = ({ }) => {
             products.map((product) => {
               return (
                 <div key={product.name}>
-                  <PhotoImages1 key={product.name} category={product} productID={product.id} />
-                  
+                  {/* <PhotoImages1 key={product.name} category={product} productID={product.id} /> */}
+                  <div>
+                    <div id={`drop`}>{product.id}</div>
+                    <div>
+                      <h2>{product.name}</h2>
+                      <h3>${product.price}.00</h3>
+                      <div className={`images`}>
+                        <img src={product.imageUrl} alt={``} width={90} />
+                      </div>
+                    </div>
+                    {product.options.map((opt, optIndex) => {
+                      return <input key={optIndex} id={`${product.id}-${optIndex}`} checked={opt.checked} type={`checkbox`} value={`option${optIndex}`} name={`${optIndex}`} onChange={(event) => handleCheck(opt, product)} />
+                    })}
+                    {/* <Panel key={id} category={category} productID={productID} /> */}
+                  </div>
                 </div>
               )
             })}
@@ -41,4 +98,3 @@ const Category = ({ }) => {
     </div>
   );
 };
-export default Category;
