1d11aa5f1b62340e174c6d06de6a8533b055257d
Joshei <joshuaeirm@gmail.com>
2024-09-04 22:10:24 -0400

    Cleaned up.

--- src/routes/category/category-component.jsx
+++ src/routes/category/category-component.jsx
@@ -43,9 +43,7 @@ let PHOTO = [
         options: [{option:'option1' , checked: false}, {option:'option2' , checked: false}, {option:'option3' , checked: false}, {option:'option4' , checked: false}],
         category: "art1",
         amtstars: "none"
-        //options: [{option:'option1' , checked: true}, {option:'option2' , checked: false}, {option:'option3' , checked: false}, {option:'option4' , checked: false}]
-  
-        //options: [ {checked: true,checked: true,checked: true,checked: true, }]/
+       
         
       },
 
@@ -59,20 +57,10 @@ let PHOTO = [
       amtstars: "none"
       },
 
-      
-  //  ],
-  //},
 ];
 
 
 let storedPanel = []
-
-
-
-
-
-
-
 //used
 var star1 = "++++";
 
@@ -121,32 +109,11 @@ export const makeCopyOfProductsWithCustomData = (arrayOfProducts, category) => {
 };
 
 
-
-
-
-//let setCheckBoxes = (chk0, chk1, chk2,chk3, PanelInfoArray) => {
-//  PanelInfoArray[0].options[0].checked = chk0;
-//  PanelInfoArray[0].options[1].checked = chk1;
-//  PanelInfoArray[0].options[2].checked = chk2;
-//  PanelInfoArray[0].options[3].checked = chk3;
-//
-//  return PanelInfoArray
-//
-//}
-
-
-
 let initialProducts = []
-
-
-
+  
   //associated with image-component
   export default function Category() {
-
-    
     let [hasrun, setHasRun] = useState(false) 
-  
-
     //setCheckBoxes(true,true,true,true, PanelInfoArray, 1)//
     const route1 = useParams()
     console.log("cp2: ", {route1})
@@ -154,10 +121,6 @@ let initialProducts = []
     
   let [products, setProducts] = useState(PHOTO) 
   storedPanel = localStorage.getItem(`panel`)
-  
-  
-  
- 
   const [state , setState] = useState([])     
  
 
@@ -182,52 +145,34 @@ let initialProducts = []
   
     
   }, [])
-
-  
-
-//  useEffect(() => {
-//  console.log( "state: " , products) 
-//  }, [products])
-
-  
   const [stars, setStars] = useState([]);
   const [showPanel, setShowPanel] = useState(false);
   //!!!!!!!!!!
-
   // And lets store the final generated array of products with everything we need back into our localstorage to sync it up//
   //localStorage.setItem(`products`, JSON.stringify(null));//
   let count = 0;
   
-  const setCheckBoxes = (check1,check2,check3,check4,infoArray,id) => {
+  const setCheckBoxesAndStars = (check1,check2,check3,check4,infoArray,id, amtountOfStars) => {
  
     infoArray[id-1].options[0].checked = check1
     infoArray[id-1].options[1].checked = check2
     infoArray[id-1].options[2].checked = check3
     infoArray[id-1].options[3].checked = check4
-    infoArray[id-1].amtstars = "testing"
-    
-
-    return infoArray
-
+    if(amtountOfStars == 1){
+      infoArray[id-1].amtstars = "One Star"
+    }
+    if(amtountOfStars == 2){
+       infoArray[id-1].amtstars = "Two Stars"
+      }
+    if(amtountOfStars == 3){
+       infoArray[id-1].amtstars = "Three Stars"
+      }
+    if(amtountOfStars == 4){
+       infoArray[id-1].amtstars = "Four Stars"
+      }
+   return infoArray
   }
 
-  const setAmountStars = (amtString, product) => {
-    let starArray = {amtstars: "one"}
-    let holdingArray = [...products]  
-
-    let prodArray = [product]
-    prodArray.push([{amtstars: "three"}])
-
-
-
-    //starArray.amtstars = amtString
-    prodArray = [product]
-    let newElement = [...prodArray, starArray ]
-    let array1 = [...products, newElement]
-    let breakpoint = 0
-
-  }
- 
   const computeStars = (e, index, howManyChecked, id, product) => {
 
       infoArray = [...products]
@@ -247,149 +192,59 @@ let initialProducts = []
       //PanelInfoArray[0].options1[0].checked = true
       if (index === 0) {
       if (infoArray[id-1].options[1].checked == true) {
-        
-        setCheckBoxes(true,false,false,false,infoArray,id)
         amtStars = 1;
-      } else if (infoArray[id - 1].options[0].checked == true) {
-        setCheckBoxes(false,false,false,false,infoArray,id)
+        setCheckBoxesAndStars(true,false,false,false,infoArray,id,amtStars)
+        } else if (infoArray[id - 1].options[0].checked == true) {
         amtStars = 0;
-      } else {
-        setCheckBoxes(true,false,false,false,infoArray,id)
+        setCheckBoxesAndStars(false,false,false,false,infoArray,id,amtStars)
+        } else {
         amtStars = 1;
-      }
+        setCheckBoxesAndStars(true,false,false,false,infoArray,id,amtStars)  
+        } 
     }
-
     if (index === 1) {
       if (infoArray[id - 1].options[2].checked == true) {
-        setCheckBoxes(true,true,false,false,infoArray,id)
-        amtStars = 2;
-      } else if (infoArray[id - 1].options[1].checked == false) {
-        setCheckBoxes(true,true,false,false,infoArray,id)
-        amtStars = 2;
-      } else {
-        setCheckBoxes(false,false,false,false,infoArray,id)
+         amtStars = 2;
+        setCheckBoxesAndStars(true,true,false,false,infoArray,id,amtStars)
+       } else if (infoArray[id - 1].options[1].checked == false) {
+         amtStars = 2;
+        setCheckBoxesAndStars(true,true,false,false,infoArray,id,amtStars)
+       } else {
         amtStars = 0;
-      }
-    }
-    
+        setCheckBoxesAndStars(false,false,false,false,infoArray,id,amtStars)
+         }
+       }
     if (index === 2) {
       if (infoArray[id - 1].options[3].checked == true) {
-        setCheckBoxes(true,true,true,false,infoArray,id)
-        amtStars = 3;
-      } else if (infoArray[id - 1].options[2].checked == true) {
-        setCheckBoxes(false,false,false,false,infoArray,id)
-        amtStars = 0;
-      } else {
-        setCheckBoxes(true,true,true,false,infoArray,id)
-        amtStars = 3;
+            amtStars = 3;
+        setCheckBoxesAndStars(true,true,true,false,infoArray,id,amtStars)
+        } else if (infoArray[id - 1].options[2].checked == true) {
+       amtStars = 0;
+        setCheckBoxesAndStars(false,false,false,false,infoArray,id,amtStars)
+       } else {
+      amtStars = 3;
+      setCheckBoxesAndStars(true,true,true,false,infoArray,id,amtStars)
       }
     }
+
     if (index === 3) {
       if (infoArray[id - 1].options[3].checked == true) {
-        setCheckBoxes(false,false,false,false,infoArray,id)
-        
         amtStars = 0;
-      } else {
-        setCheckBoxes(true,true,true,true,infoArray,id)
+        setCheckBoxesAndStars(false,false,false,false,infoArray,id,amtStars)
+        } else {
         amtStars = 4;
+        setCheckBoxesAndStars(true,true,true,true,infoArray,id,amtStars)
+        }
       }
-
+      setProducts(infoArray)
+     
+      localStorage.setItem(`panel`, JSON.stringify(infoArray));
       
-    }
-
-
-    //////////////
-          //UNCOMMENT THESE WITH A FILTER, TO CHANGEE TO NEW DATA
-          //setProducts((products) => [...products,{ id: 7, name:"name4" ,  imageUrl: require ("./test.png"),  price: 6, options: [{option:'option1' , checked: false}, {option:'option2' , checked: true}, {option:'option3' , checked: false}, {option:'option4' , checked: false}]   }
-          //]);
-         
-          ////setProducts((products) => [...products, { id: 100, name:"name1b",   imageUrl: require ("./test.png") , price: 6, options: [{option:'option1' , checked: true}, {option:'option2' , checked: false}, {option:'option3' , checked: false}, {option:'option4' , checked: false}]   }
-          ////]);
-   
+      storedPanel = localStorage.getItem(`panel`)
+    
+    //setProducts((products) => [...products, { id: 100, name:"name1b",   imageUrl: require ("./test.png") , price: 6, options: [{option:'option1' , checked: true}, {option:'option2' , checked: false}, {option:'option3' , checked: false}, {option:'option4' , checked: false}]}
     //localStorage.setItem(`products`, JSON.stringify(PanelInfoArray));
-    //console.log("pan: ", PanelInfoArray)
-    ///////////////
-
-    handleCheck(true, product, howManyChecked, infoArray);
-    
-  };
-  
-  let panelArray2 = [];
-
-  //!!!
-  
-  let indexCount = 0;
-  //!!!
-
-  let panelArrayWithSameIDsChanged = []
- 
-  
-
-  //put text in element
-  const handleCheck = (option, product, amtStars, withOptionsArray) => {
-   
-    let amtString = "";
-    switch (amtStars) {
-      case 1:
-       amtString = "One Star";
-        break;
-      case 2:
-        amtString = "Two Stars";
-        break;
-      case 3:
-        amtString = "Three Stars";
-        break;
-      case 4:
-        amtString = "Four Stars";
-        break;
-      default:
-        amtString = "none";
-    }
-
-
-    //setAmountStars(amtString, product)
-
-    //let array1 = [{ amtstars: "one"}]
-    //array1[product.id - 1].amtStars = amtString
-    //let panelElement = [...withOptionsArray, ...array1 ]
-/*  
-var panelElement = {
-
-  id:product.id,
-  name: "name100",
-  //imageUrl: require ("./test.png"),
-  imageUrl: product.imageUrl,
-  price:  product.price,
-  options: [{option:'option1' , checked: check1}, {option:'option2' , checked: check2}, {option:'option3' , checked: false}, {option:'option4' , checked: false}],
-  category: "art2",
-  amtstars: amtString
-};
-*/
-
-  setProducts(withOptionsArray)
-
-
-    
-
-    ////////////THIS IS FOR DISPLAYING PANEL///////////////////////////////////////////////////////////
-    //let update = []
-    ////asynchronous
-    //let anotherPanel = [...update]
-    ////add an element to array
-    //panelArrayWithSameIDsChanged = anotherPanel.map(element => element.id !== product.id ? element : panelElement)
-    //console.log("products: ", products)
-    //localStorage.clear()
-    
-    //localStorage.setItem(`panel`, JSON.stringify(panelArrayWithSameIDsChanged));
-    localStorage.setItem(`panel`, JSON.stringify(withOptionsArray));
-    
-    storedPanel = localStorage.getItem(`panel`)
-
-    let breakpoint_here = 1
-
-    /////////////////////////////////////////
-}
-  
+   };
   return (
     <div>
       <br></br><br></br>
