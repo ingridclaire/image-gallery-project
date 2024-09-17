a22961c9fb808cc351820fe3930841fb86b76f2e
Joshei <joshuaeirm@gmail.com>
2024-09-16 20:32:10 -0400

    Done with project, next prettier.

--- src/routes/category/category-component.jsx
+++ src/routes/category/category-component.jsx
@@ -626,19 +626,14 @@ artPiecesOfCategoryArray  = products.filter((element) => element.category === im
           {
             
             artPiecesOfCategoryArray.map((product) => {
-                let ID = product.id;
-                let productID = `product-${product.id}`;
-                let productDetailsID = `${productID}-details`;
-                console.log("mapping products: ", product)
-                
+              let ID = product.id;
                 return (
-                  
-                 <div id={productID} className={`product`} key={product.imageUrl}>
-                    <div id={`drop`}>{product.id+1}</div>
-                    <div id={productDetailsID}>
+                    <div key={product.imageUrl}>
+                    <div>{product.id+1}</div>
+                    <div>
                       <h2>{product.name}</h2>
                       <h3>${product.price}.00</h3>
-                      <div className={`images`}>
+                      <div>
                         
                         <img
                           type="Image"
@@ -652,20 +647,20 @@ artPiecesOfCategoryArray  = products.filter((element) => element.category === im
                       </div>
                       
                     </div>
-                     <div className="optionsContainer">
+                     <div>
                       
                      {product.options.map((opt, optIndex) => {
                         let newOptIndex = optIndex + 1;
-                        let productOptionValue = `option${newOptIndex}`;
-                        let productOptionID = `${productID}-option-${newOptIndex}`;
+                        
+                       
                         
                         return (
                           <input
                             key={optIndex}
-                            id={productOptionID}
+                           
                             checked={opt.checked}
                             type={`checkbox`}
-                            value={productOptionValue}
+                           
                             name={newOptIndex}
                             onChange={e => computeStars(e,  newOptIndex, newOptIndex, ID, product)
                           
