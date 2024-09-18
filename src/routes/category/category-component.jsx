f57ab5652b904bf840a94bc40d8897de91887eab
Joshei <joshuaeirm@gmail.com>
2024-09-18 11:31:20 -0400

    Quick change.

--- src/routes/category/category-component.jsx
+++ src/routes/category/category-component.jsx
@@ -16,7 +16,7 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-  }[buttonType]);   // what is this?
+  }[buttonType]);   // what is this line?
 
 */
 ///////////////////////////////////////////////
